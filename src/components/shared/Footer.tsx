e411e1fa56b5dd4c9222179d9382bb1cf7f46bbc
Adrian W <107289257+tyk-adrian@users.noreply.github.com>
2022-10-05 13:49:56 +0800

    Adrian/add dmca link (#18)
    * add dmca link in footer
    
    * re-add vambies

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,8 +1,12 @@
 import styled from '@emotion/styled'
 import React from 'react'
 
+import content from '../../content/default.yaml'
+
 export const Container = styled.footer<Props>`
+  padding: ${({ theme }) => theme.spacing(8, 0)};
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   background-color: ${({ isLight, theme }) =>
@@ -11,11 +15,19 @@ export const Container = styled.footer<Props>`
 `
 
 export const Text = styled.span`
-  padding: ${({ theme }) => theme.spacing(8, 0)};
   font-weight: 300;
   text-transform: uppercase;
   letter-spacing: 4px;
   font-size: 0.75rem;
+`
+
+export const Link = styled.a`
+  opacity: 0.7;
+  margin-top: ${({ theme }) => theme.spacing(2)};
+  font-weight: 300;
+  font-size: 0.75rem;
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.common.black};
 `
 
 interface Props {
@@ -26,6 +38,9 @@ export function Footer({ isLight = true }: Props) {
   return (
     <Container isLight={isLight}>
       <Text>© Taiyaki Studios</Text>
+      <Link href={content.dmca_policy_link} target="_blank" rel="noopener noreferrer">
+        DMCA Policy
+      </Link>
     </Container>
   )
 }
